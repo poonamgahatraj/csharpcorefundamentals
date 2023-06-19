9fd0d9051f2e9892a377d557f19045a1afbf0c5a
poonam <poonamgtrz@gmail.com>
2023-06-19 10:23:21 +0545

    program.cs

--- csharp fundamentals/csharp fundamentals/Program.cs
+++ csharp fundamentals/csharp fundamentals/Program.cs
@@ -13,44 +13,76 @@ namespace csharp_fundamentals
         static void
          Main(string[] args)
         {
-            List<int> a = new List<int>();
-            a.Add(4);
-            a.Add(5);
             List<List<int>> matrix = new List<List<int>>();
-            List<int> row1 = new List<int>() { 1, 2, 3 };
-            List<int> row2 = new List<int>() { 7, 12, 33 };
-            List<int> row3 = new List<int>() { 17, 212, 33 };
+            List<int> row1 = new List<int> { 1, 2, 3 };
+            List<int> row2 = new List<int> { 4, 5, 6 };
+            List<int> row3 = new List<int> { 7, 8, 9 };
 
             matrix.Add(row1);
             matrix.Add(row2);
             matrix.Add(row3);
 
             for (int i = 0; i < matrix.Count; i++)
-
             {
+                for (int j = 0; j < 3; j++)
 
-                for (i = 0; i < matrix.Count; i++)
-                    Console.WriteLine(matrix[0][0]);
+                    Console.Write(matrix[i][j]);
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+
+            List<int> LeftDiagonal = new List<int>();
+            for (int i = 0; i <= 0; i++)
+            {
+                Console.WriteLine(matrix[0][0]);
                 Console.WriteLine(matrix[1][1]);
                 Console.WriteLine(matrix[2][2]);
             }
+            LeftDiagonal.Add(matrix[0][0]);
+            LeftDiagonal.Add(matrix[1][1]);
+            LeftDiagonal.Add(matrix[2][2]);
+
+            Console.WriteLine();
+
+
+            List<int> RightDiagonal = new List<int>();
+            for (int i = 0; i <= 0; i++)
+            {
+                Console.WriteLine(matrix[0][2]);
+                Console.WriteLine(matrix[1][1]);
+                Console.WriteLine(matrix[2][0]);
+            }
+            RightDiagonal.Add(matrix[0][2]);
+            RightDiagonal.Add(matrix[1][1]);
+            RightDiagonal.Add(matrix[2][0]);
+
+            Console.WriteLine();
+
+
+            int sum1 = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                sum1 = LeftDiagonal[i] + sum1;
+            }
+            Console.WriteLine(sum1);
+            Console.WriteLine();
+
+
+            int sum2 = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                sum2 = RightDiagonal[i] + sum2;
+            }
+            Console.WriteLine(sum2);
+            Console.WriteLine();
+
+            int sub = 0;
+            {
+                sub = sum1 - sum2;
+                Console.WriteLine(sub);
+            }
         }
-
-    
-        
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-
     }
 }
