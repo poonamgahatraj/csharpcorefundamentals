ff791eb04871bec8281f9d07428309ae335622e5
poonam <poonamgtrz@gmail.com>
2023-06-29 21:38:08 +0545

    program.cs

--- csharp fundamentals/csharp fundamentals/Program.cs
+++ csharp fundamentals/csharp fundamentals/Program.cs
@@ -14,43 +14,23 @@ namespace csharp_fundamentals
          Main(string[] args)
         {
             {
-                PeopleInformation x = Survey();
-                Console.WriteLine(x);
+                int num;
+                Console.WriteLine("enter the number");
+                num = Convert.ToInt32(Console.ReadLine());
 
+                for (int i = 2; i < num; i++)
+                {
+                    if (i % 2 != 0)
+
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
             }
 
-            // You can define other methods, fields, classes and namespaces here
-
-            static PeopleInformation Survey()
-            {
-                string name;
-                int age;
-                string occupation;
-                Console.WriteLine("Enter person name");
-                name = Console.ReadLine();
-                Console.WriteLine("Enter person age");
-                age = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter person occupation");
-                occupation = Console.ReadLine();
-                PeopleInformation pi = new PeopleInformation();
-                pi.Name = name;
-                pi.Age = age;
-                pi.Occupation = occupation;
-
-                return pi;
-            }
-        }
-
-            class PeopleInformation
-        {
-            public string Name;
-            public int Age;
-            public string Occupation;
 
         }
-
-
-
     }
 }
 
